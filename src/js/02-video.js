0d2c4ebd25b7eefd2155073e1f690e6b5da27639
Tripoli80 <Tripoli80@gmail.com>
2022-06-17 09:17:53 +0200

    const ITEMKEY
    somthing optimization

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,13 @@
-import Player from '@vimeo/player';// подгружаем плеер 
-const _ = require('lodash');//подгружаем библиотеку тормозов
+import Player from '@vimeo/player'; // подгружаем плеер
+
+const throttle = require('lodash.throttle'); //подгружаем библиотеку тормозов
 
 const iframe = document.querySelector('iframe'); // ищим обьект по тегу или класу
-const player = new Player(iframe);//создаем обьект для управлениее найденного iframe
-
+const player = new Player(iframe); //создаем обьект для управлениее найденного iframe
+const SAVETIMEKEY = 'videoplayer-current-time';
 //считываем сохраненные данные
 //проверяем напустое значение и записываем в обьект плеера сохраненный промеж времени
-const sevedTime = localStorage.getItem('videoplayer-current-time');
+const sevedTime = localStorage.getItem(SAVETIMEKEY);
 const parsedSettings = JSON.parse(sevedTime);
 const timeStart = parsedSettings ? parsedSettings : 0;
 player.setCurrentTime(timeStart);
@@ -14,7 +15,8 @@ player.setCurrentTime(timeStart);
 //отслеживаем и записываем временную шкалу не чаще чем 1 раз в 1000 мс
 player.on(
   'timeupdate',
-  _.throttle(e => {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+  throttle(e => {
+    localStorage.setItem(SAVETIMEKEY, JSON.stringify(e.seconds));
+    console.log(SAVETIMEKEY);
   }, 1000)
 );
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,15 @@
-const _ = require('lodash'); //подгружаем библиотеку тормозов
+const throttle = require('lodash.throttle'); //подгружаем библиотеку тормозов
 const validator = require('validator');
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', _.throttle(savedInputForm, 500));
+const TEMPSTRING = 'feedback-form-state';
+
 const data = {};
+form.addEventListener('input', throttle(savedInputForm, 500));
 readAndWrite();
 //read data & write data
 function readAndWrite() {
   try {
-    const tempRead = localStorage.getItem('feedback-form-state');
+    const tempRead = localStorage.getItem(TEMPSTRING);
     const readData = JSON.parse(tempRead);
     // readData.email="asdas";
     data.email = readData.email;
@@ -25,8 +27,9 @@ function savedInputForm(e) {
   }
   const keyName = e.target.name;
   data[keyName] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  localStorage.setItem(TEMPSTRING, JSON.stringify(data));
 }
+
 form.addEventListener('click', e => {
   if (e.target.nodeName !== 'BUTTON') {
     return;
@@ -36,10 +39,9 @@ form.addEventListener('click', e => {
   if (validator.isEmail(data.email)) {
     console.log('send data to server');
     console.table(data);
-    localStorage.clear();
+    localStorage.removeItem(TEMPSTRING);
     readAndWrite();
   } else {
-    alert('e-mail not correct');
+    alert('E-mail not correct!');
   }
-  // console.log(e.button);
 });
